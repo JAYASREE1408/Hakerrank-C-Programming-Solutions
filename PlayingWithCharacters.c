9bab45d17747df578795ebbf01b9c552d46623b1
JAYASREE T <jayasreet2112@gmail.com>
2020-09-29 00:53:15 +0530

    Playing With Characters

---
+++ PlayingWithCharacters.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch, s[20],t,sen[100];
+    scanf("%c",&ch);
+    scanf("%s",s);
+    scanf("%c",&t);
+    scanf("%[^\n]", sen);
+    printf("%c\n",ch);
+    printf("%s\n",s);
+    printf("%s",sen);
+    return 0;
+}
